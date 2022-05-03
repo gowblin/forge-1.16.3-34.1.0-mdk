59af7ebcfc7aae661e0234c44784264204ceff52
maya <gowblin>
2022-05-02 17:18:40 -0700

    v. 1

--- src/main/java/com/udemy/mccourse/block/ModBlocks.java
+++ src/main/java/com/udemy/mccourse/block/ModBlocks.java
@@ -9,6 +9,7 @@ import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.common.ToolType;
 
 import java.util.function.Supplier;
 
